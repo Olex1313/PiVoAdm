cddccc5d0b51de7de37fecc8c7502cfd7ed1b0ab
Olex1313 <aa1limonov@gmail.com>
2022-05-08 20:43:24 +0300

    :tada: reboot to spring

---
+++ src/test/java/ru/llm/pivocore/PiVoCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.llm.pivocore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiVoCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
